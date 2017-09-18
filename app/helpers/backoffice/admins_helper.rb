09334f020951f11e8326289ae655a10661edea1a
Anderson M. de Almeida <arkanntus@gmail.com>
2017-09-17 22:27:18 -0300

    #42 Devise Errors & Collection Radio Buttons

--- app/helpers/backoffice/admins_helper.rb
+++ app/helpers/backoffice/admins_helper.rb
@@ -1,2 +1,13 @@
 module Backoffice::AdminsHelper
+  OptionsForRole = Struct.new(:id, :description)
+
+  def options_for_roles
+    options = []
+    Admin.roles_i18n.each do |key, value|
+      options.push(OptionsForRole.new(key, value))
+    end
+
+    options
+  end
+
 end
