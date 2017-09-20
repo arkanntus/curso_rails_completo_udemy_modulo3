2f6e823aef48d83087415d20c0723d2f461e7c9a
Anderson M. de Almeida <arkanntus@gmail.com>
2017-09-19 22:13:14 -0300

    #44 Action Mailer

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -1,6 +1,6 @@
 class Backoffice::AdminsController < BackofficeController
   before_action :set_admin, only: [:edit, :update, :destroy]
-  after_action :verify_authorized, only: :new
+  after_action :verify_authorized, only: [:new, :destroy]
   # after_action :verify_policy_scoped, only: :new     #nao funcionou como esperado#
 
   def index
@@ -36,6 +36,7 @@ class Backoffice::AdminsController < BackofficeController
   end
 
   def destroy
+    authorize @admin
     admin_name = @admin.name
     if @admin.destroy
       redirect_to backoffice_admins_path, notice: I18n.t('messages.destroyed_with', item: admin_name)
