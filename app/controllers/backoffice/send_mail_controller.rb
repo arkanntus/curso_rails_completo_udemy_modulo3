94801987b8ead9e6a1863bbfd15ef874bfc6f976
Anderson M. de Almeida <arkanntus@gmail.com>
2017-09-23 19:37:36 -0300

    #48 Enviando uma mensagem/email através do modal

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -1,13 +1,17 @@
 class Backoffice::SendMailController < ApplicationController
   def edit
     @admin = Admin.find(params[:id])
-    
     respond_to do |format|
       format.js
     end
   end
 
   def create
-    
+    #send_message(current_admin, recipient, subject, message)
+    AdminMailer.send_message(current_admin, params[:'recipient-text'], 
+                             params[:'subject-text'], params[:'message-text']).deliver_now
+    respond_to do |format|
+      format.js
+    end
   end
 end
