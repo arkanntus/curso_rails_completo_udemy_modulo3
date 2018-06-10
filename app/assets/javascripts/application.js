691edc91478896d1b65cbc52512773f3a6727809
Anderson M. de Almeida <arkanntus@gmail.com>
2018-06-10 18:49:49 -0300

    #75 Carregando o detalhe do anúncio via AJAX

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.growl
+
+/* Spin */
+$(document).ready(function() {
+    $(document)
+    .ajaxStart(function () {
+        $('#global-spin').fadeIn('slow');
+    })
+    .ajaxStop(function () {
+        $('#global-spin').fadeOut('slow');
+    });
+});
