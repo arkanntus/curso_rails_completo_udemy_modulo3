f4cf333c8c7b730cd05e3123c9267c501c52b2de
Anderson M. de Almeida <arkanntus@gmail.com>
2018-06-22 11:48:33 -0300

    #92 Corrigindo rotas do Devise após login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_filter :store_current_location, :unless => :devise_controller?
   #Pundit
   include Pundit
 
@@ -30,5 +31,9 @@ class ApplicationController < ActionController::Base
     def user_not_authorized
       flash[:alert] = "Você não está autorizado a executar essa ação."
       redirect_to(request.referrer || root_path)
+    end
+
+    def store_current_location
+      store_location_for(:member, request.url)
     end
 end
