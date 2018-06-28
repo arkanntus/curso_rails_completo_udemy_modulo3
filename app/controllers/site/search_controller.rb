6141780ade8546cdd52cc6ae6cef2187a0884d99
Anderson M. de Almeida <arkanntus@gmail.com>
2018-06-28 19:47:03 -0300

    Cookies Rails

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,7 @@
 class Site::SearchController < SiteController
     def ads
+        cookies[:search_term] = params[:q]
+        
         @categories = Category.order_by_description
         # @ads = Ad.where(title: params[:q])
         @ads = Ad.search(params[:q], params[:page])
